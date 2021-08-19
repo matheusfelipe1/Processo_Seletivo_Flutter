9b9ea28efbecee9f56461c262aeea53dc85eea86
matheusfelipe1 <matheusfel13@gmail.com>
2021-08-19 16:15:57 -0300

    Adicionando banco de dados e suas funções

--- backend/api/data.js
+++ backend/api/data.js
@@ -1,9 +1,24 @@
 const axios = require('axios');
 
 module.exports = app => {
+    
     const mostra = async (req, res, next) => {
         await axios.get('https://api.covid19api.com/country/brazil')
-            .then(resp => res.send(resp.data))
+            .then(resp => {
+               
+                for (var i = 0; i <= resp.data.length; i++)  {
+                   const validate =  app.db('covidbrasil').where({'ID': resp.data[i]['ID']}).first()
+                   if (validate == true) {
+                       return
+                   }
+
+                    app.db('covidbrasil')
+                    .insert(resp.data[i])
+                    .then(() => res.send('deu certo'))
+                    .catch(err => res.send('erro!'))
+                }
+
+            })
 
     }  
 
---
+++ backend/knexfile.js
@@ -0,0 +1,19 @@
+
+
+module.exports = {
+	client: 'mysql',
+	connection: {
+        host : '127.0.0.1',
+        port: 3306,
+        database: 'processo_seletivo_flutter',
+        user: 'root',
+        password: 'Omgksyhd'
+    },
+	pool: {
+		min: 2,
+		max: 10
+	},
+	migrations: {
+		tableName: 'knex_migrations'
+	}
+};
