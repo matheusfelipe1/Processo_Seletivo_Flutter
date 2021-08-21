827ae6440fad52f385b29f76e70c7f22ca3d7d64
matheusfelipe1 <matheusfel13@gmail.com>
2021-08-21 18:50:37 -0300

    adicionando grafico de legenda linear

---
+++ processo_seletivo_flutter/lib/components/componente_legenda/Componente_legenda.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class ComponentLegenda extends StatelessWidget {
+  Widget RenderizaLegendas(
+    String textoLegenda,
+    String textoLegendaMedia,
+    Color legenda,
+    Color legendaMedia,
+  ) {
+    return Container(
+      margin: EdgeInsets.only(top: 5, bottom: 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            child: Row(
+              children: [
+                SizedBox(
+                  height: 20,
+                  width: 20,
+                  child: Container(
+                    color: legenda,
+                  ),
+                ),
+                Container(
+                  child: Text(textoLegenda),
+                  margin: EdgeInsets.only(left: 7, right: 15),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            child: Row(
+              children: [
+                SizedBox(
+                  height: 20,
+                  width: 20,
+                  child: Container(
+                    color: legendaMedia,
+                  ),
+                ),
+                Container(
+                  child: Text(textoLegendaMedia),
+                  margin: EdgeInsets.only(left: 7),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+      children: [
+        RenderizaLegendas('Confirmados', 'Media Móvel Confirmados', Colors.blue,
+            Colors.yellow),
+        RenderizaLegendas(
+            'Mortes', 'Media Móvel Mortes ', Colors.red, Colors.yellow),
+        RenderizaLegendas('Recuperados', 'Media Móvel Confirmado', Colors.green,
+            Colors.yellow),
+      ],
+    ));
+  }
+}
