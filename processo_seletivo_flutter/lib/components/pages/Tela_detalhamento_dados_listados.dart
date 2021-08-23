2552296e7b39721ae9401f736cb87a5dc64714fe
matheusfelipe1 <matheusfel13@gmail.com>
2021-08-22 21:38:32 -0300

    Iniciando tela de detalhamento

--- processo_seletivo_flutter/lib/components/pages/Tela_detalhamento_dados_listados.dart
+++ processo_seletivo_flutter/lib/components/pages/Tela_detalhamento_dados_listados.dart
@@ -1,8 +1,13 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:processo_seletivo_flutter/services/Services_detalhamento.dart';
+import 'package:http/http.dart' as http;
 
 class TelaDetalhamentoDadosListados extends StatefulWidget {
   final data;
+
   TelaDetalhamentoDadosListados({this.data});
   @override
   _TelaDetalhamentoDadosListadosState createState() =>
@@ -11,20 +16,46 @@ class TelaDetalhamentoDadosListados extends StatefulWidget {
 
 class _TelaDetalhamentoDadosListadosState
     extends State<TelaDetalhamentoDadosListados> {
+  var dadosObjetoSelecionado;
+
+  var dataParaEnvio;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     DateTime data = DateTime.parse(widget.data);
-    var data2 = DateFormat("yyyy-MM-dd").format(data);
-    print(data2);
+    setState(() {
+      dataParaEnvio = DateFormat("yyyy-MM-dd").format(data);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          backgroundColor: Colors.purple[900], title: Text('Detalhamento')),
-    );
+    return FutureBuilder<dynamic>(
+        future: obterDetalhamento(http.Client(), dataParaEnvio: dataParaEnvio),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            dadosObjetoSelecionado = snapshot.data[1];
+
+            return Scaffold(
+              appBar: AppBar(
+                backgroundColor: Colors.purple[900],
+                title: Text('Detalhamento'),
+              ),
+              body: Column(
+                children: [],
+              ),
+            );
+          } else {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text('Detalhamento'),
+              ),
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+        });
   }
 }
